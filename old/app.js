b47d6cc1f6bad48debb39b3fb932e9fff54fafed
Bill.zhanxg <76557923+bill-zhanxg@users.noreply.github.com>
2022-03-05 12:08:29 +1100

    updates

---
+++ old/app.js
@@ -0,0 +1,16 @@
+
+function open() {
+    console.log("UHHHH BRUH");
+    alert("test");
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+function close() {
+    console.log("UHHHH BRUH");
+    alert("test");
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
